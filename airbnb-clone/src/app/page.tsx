3e32a98b661efafa9d3fe57c4ccd8184ce4a4e1d
HaziqAJieq <muhd.haziq.khair@gmail.com>
2024-07-10 09:55:03 +0800

    banner progress

--- airbnb-clone/src/app/page.tsx
+++ airbnb-clone/src/app/page.tsx
@@ -2,16 +2,17 @@
 
 import React from "react";
 import Header from "./component/Header";
+import Banner from "./component/Banner";
 
 export default function Home() {
   return (
     <div className="">
      
      {/* create a Header */}
-     
      <Header/>
-     {/* Banner */}
-    
+
+      {/* Banner */}
+    <Banner/>
    
     </div>
   );
